4d121de785413f6b1e6b58e120b9377cb9c79403
hyebinjo <hyebinjo91@gmail.com>
2021-12-15 21:45:45 +0900

    Create project

---
+++ main.js
@@ -0,0 +1,9 @@
+let str = `
+010-1234-5678
+thesecon@gmail.com
+https://www.omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+abbcccdddd
+http://localhost:1234`
+
+console.log(str.match(/d{2,3}/g));//['dddd']
